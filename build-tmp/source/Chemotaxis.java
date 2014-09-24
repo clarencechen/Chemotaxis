572bac54cca7d550570c011b45d8d736ed70c6b0
unknown <clchen@697-334-Thorin.sf.k12.internal>
2014-09-24 10:22:55 -0700

    Chemotaxis

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 int c = 0;
 Bacteria[] bob = new Bacteria[16];
 Food particle;
-void setup()   
+public void setup()   
 {
 	size(600,600);
 	background(360);
@@ -12,7 +28,7 @@ void setup()
 	}
 	particle = new Food(150,150);
 }   
-void draw()   
+public void draw()   
 {
 	fill(360);
 	rect(0,0,600,600);
@@ -23,7 +39,7 @@ void draw()
 		particle.show();
 	}
 }
-void mousePressed()
+public void mousePressed()
 {
 	if(mouseButton == LEFT)
 	{
@@ -37,7 +53,7 @@ void mousePressed()
 		particle = new Food(mouseX -mouseX%2,mouseY -mouseY%2);
 	}
 }
-void keyPressed()
+public void keyPressed()
 {
 	if (key == CODED)
 	{
@@ -78,7 +94,7 @@ class Bacteria
 		this.col = 0;
 		sz = 2;
 	}
-	void walk()
+	public void walk()
 	{
 		if(abs(particle.x-x)<2 && abs(particle.y-y)<2 && particle.quan>0)
 		{
@@ -124,13 +140,13 @@ class Bacteria
 			sy = y;
 		}
 	}
-	void feed()
+	public void feed()
 	{
 			particle.quan--;
 			sz++;
 			col = particle.col;
 	}
-	void show()
+	public void show()
 	{
 		stroke(0);
 		fill(3*col,360,180);
@@ -150,7 +166,7 @@ class Food
 		quan = (int)(Math.random()*16)+1;
 		this.col = c;
 	}
-	void show()
+	public void show()
 	{
 		if(particle.quan>0)
 		{
@@ -159,4 +175,13 @@ class Food
 			ellipse(x,y,quan,quan);
 		}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
